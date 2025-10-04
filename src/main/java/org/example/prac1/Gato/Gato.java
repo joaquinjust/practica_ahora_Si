e432c2c89a7df73e4a723b22977134aa79806ca9
unknown <jjustoguillen@gmail.com>
2025-10-04 01:40:51 -0500

    cambiosgatooo

--- src/main/java/org/example/prac1/Gato/Gato.java
+++ src/main/java/org/example/prac1/Gato/Gato.java
@@ -6,13 +6,21 @@ import jakarta.persistence.Id;
 
 public class Gato {
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String perro;
     private String raza;
     private String vacuna;
+
+    //t23t23t23t
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id2;
+    private String p3erro;
+    private String ra2za;
+    private String vaceuna;
+
 
 
 //"LUCIANA QUE ERGA ES ESTO TODOS HEMSO EHCHO 100 COSAS EN SU WEBADA Y TU ANDA PRME WENTITRRY NNOMÁS NJODER"
